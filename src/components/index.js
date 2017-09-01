dad867221b185c8fdbd911a0dd146b8f95d77f66
Ranjan Kumar Pandey <ranjankp@pune.smartek21.st21>
2017-09-01 18:51:41 +0530

    Initial commit for Login App

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from './Header';
+import Authentication from './Authentication';
+
+
+export { Header, Authentication};
---
+++ src/containers/Home.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingState: false,
+            initiallyLoaded: false
+        };
+    }
+
+  
+    render() {
+       
+        return (
+            <div className="wrapper">
+            </div>
+        );
+    }
+}
+
+Home.PropTypes = {
+    username: React.PropTypes.string
+};
+
+Home.defaultProps = {
+    username: undefined
+};
+
+const mapStateToProps = (state) => {
+    return {
+        isLoggedIn: state.authentication.status.isLoggedIn,
+        postStatus: state.memo.post,
+        currentUser: state.authentication.status.currentUser,
+        memoData: state.memo.list.data,
+        listStatus: state.memo.list.status,
+        isLast: state.memo.list.isLast,
+        editStatus: state.memo.edit,
+        removeStatus: state.memo.remove,
+        starStatus: state.memo.star
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        memoPostRequest: (contents) => {
+            return dispatch(memoPostRequest(contents));
+        },
+        memoListRequest: (isInitial, listType, id, username) => {
+            return dispatch(memoListRequest(isInitial, listType, id, username));
+        },
+        memoEditRequest: (id, index, contents) => {
+            return dispatch(memoEditRequest(id, index, contents));
+        },
+        memoRemoveRequest: (id, index) => {
+            return dispatch(memoRemoveRequest(id, index));
+        },
+        memoStarRequest: (id, index) => {
+            return dispatch(memoStarRequest(id, index));
+        }
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
